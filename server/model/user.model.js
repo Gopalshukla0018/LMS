4b0ff13384f5a1bfbe89e211526ee73fc55edd67
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-09-06 22:16:34 +0530

    controller

--- server/model/user.model.js
+++ server/model/user.model.js
@@ -9,7 +9,9 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    password: { type: String, required: true },
+    password: { 
+      type: String,
+       required: true },
     role: {
       type: String,
       enum: ["student", "instructor"],
