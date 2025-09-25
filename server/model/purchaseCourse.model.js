586f7bd063df247e14408345400fb26a1ea72cb3
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-09-25 21:40:31 +0530

    sample commit

--- server/model/purchaseCourse.model.js
+++ server/model/purchaseCourse.model.js
@@ -1,40 +1,30 @@
 import mongoose from "mongoose";
 
-const purchasedCourseSchema = new mongoose.Schema(
-  {
-    courseId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Course",
-      required: true,
-    },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-
-    amount: {
-      type: Number,
-      required: true,
-    },
-    payment_status: {
-      type: String,
-      enum: ["pending", "success", "failed"],
-      default: "pending",
-    },
-    order_id: {
-      type: String,
-      required: true,
-    },
-
-    payment_id: {
-      type: String,
-    },
+const purchaseSchema = new mongoose.Schema({
+  user: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User',
+    required: true 
   },
-  { timestamps: true }
-);
+  course: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Course',
+    required: true 
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  order_id: {
+    type: String,
+    required: true,
+  },
+  payment_status: {
+    type: String,
+    enum: ["pending", "success", "failed"],
+    default: "pending",
+  },
+}, { timestamps: true });
 
-export const PurchasedCourse = mongoose.model(
-  "PurchasedCourse",
-  purchasedCourseSchema
-);
+
+export const Purchase = mongoose.model("Purchase", purchaseSchema);
