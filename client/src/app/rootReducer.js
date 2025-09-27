bbfaf873e4d3fc0a0a4040ddef73322f9a0a9749
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-09-27 19:28:16 +0530

    feat: add Course Progress page

--- client/src/app/rootReducer.js
+++ client/src/app/rootReducer.js
@@ -3,10 +3,12 @@ import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import { courseApi } from "@/features/api/courseApi";
 import paymentApi from "@/features/api/paymentApi";
+import { courseProgressApi } from "@/features/api/courseProgressApi";
 const rootReducer = combineReducers({
     [authApi.reducerPath]:authApi.reducer,
     [courseApi.reducerPath]:courseApi.reducer,
      [paymentApi.reducerPath]: paymentApi.reducer,
+     [courseProgressApi.reducerPath]: courseProgressApi.reducer,
     auth:authReducer
 });
 export default rootReducer;
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -5,15 +5,24 @@ import { authApi } from "../features/api/authApi";
 import { courseApi } from "@/features/api/courseApi";
 
 import paymentApi from "@/features/api/paymentApi";
+import { courseProgressApi } from "@/features/api/courseProgressApi";
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (defaultMiddleware) =>
-    defaultMiddleware().concat(authApi.middleware,courseApi.middleware, paymentApi.middleware),
+    defaultMiddleware().concat(
+      authApi.middleware,
+      courseApi.middleware,
+      paymentApi.middleware,
+      courseProgressApi.middleware
+
+    ),
 });
 
-const initializeApp=async()=>{
-  await store.dispatch(authApi.endpoints.loardUser.initiate({},{forceRefetch:true}))
-}
+const initializeApp = async () => {
+  await store.dispatch(
+    authApi.endpoints.loardUser.initiate({}, { forceRefetch: true })
+  );
+};
 
 initializeApp();
