9be74c9c5751096cf9f64ff07691549e05ebab26
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-09-30 16:07:59 +0530

    fic: generate token

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,19 +1,30 @@
 import jwt from "jsonwebtoken";
 
-export const generateToken = (res, user, message) => {
-  const Token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
-    expiresIn: "1d",
+const generateToken = (res, user, message) => {
+  const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+    expiresIn: "15d",
   });
-  return res
-    .status(200)
-    .cookie("token", Token, {
-      httpOnly: true,
-      sameSite: "strict",
-      maxAge: 24 * 60 * 60 * 1000, //for 1 day
-    })
-    .json({
-      sucess: true,
-      message,
-      user,
-    });
+
+  // Cookie options ko define karein
+  const cookieOptions = {
+    httpOnly: true,
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+  
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  };
+
+  res.cookie("token", token, cookieOptions).status(200).json({
+    sucess: true,
+    message,
+    user: {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      photoUrl: user.photoUrl,
+    },
+  });
 };
+
+export default generateToken;
