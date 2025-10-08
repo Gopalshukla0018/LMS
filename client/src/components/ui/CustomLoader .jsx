fa15980ae36022a223eac733a894b6b6fbbd0b8d
gopalshukla0018 <gopalshukla0018@gmail.com>
2025-10-09 01:09:42 +0530

    fix(CustomLoader): correct typo in useLoardUserQuery to useLoadUserQuery

--- client/src/components/ui/CustomLoader .jsx
+++ client/src/components/ui/CustomLoader .jsx
@@ -38,8 +38,8 @@ const CustomLoader = ({ children }) => {
             >
               Loading Courses...
             </motion.h2>
-            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
-              {Array.from({ length: 2 }).map((_, idx) => (
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 ">
+              {Array.from({ length: 12 }).map((_, idx) => (
                 <motion.div key={idx} variants={fadeVariants}>
                   <CoursesSkeleton />
                 </motion.div>
@@ -55,3 +55,4 @@ const CustomLoader = ({ children }) => {
 };
 
 export default CustomLoader;
+
