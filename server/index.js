97bc6c256f99dff9f6da360d1c585515b6e99ce0
Gopal Shukla <gopalshukla0018@gmail.com>
2025-11-03 18:40:30 +0530

    feat: O auth completed

--- server/index.js
+++ server/index.js
@@ -4,17 +4,25 @@ import connectDB from "./database/db.js";
 import userRoute from "./routes/user.route.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+
+// OAuth imports
+import session from "express-session";
+import passport from "passport";
+import { connectPassport } from "./config/passport.js";
+
+// routes import 
 import courseRoutes from "./routes/course.route.js";
 import mediaRoute from "./routes/media.routes.js";
 import paymentRouter from "./routes/payment/payment.route.js";
 import courseProgressRoute from "./routes/courseProgress.route.js";
 import adminRoutes from "./routes/admin/admin.routes.js";
-import superAdminRoutes from "./routes/superAdmin.route.js"
+import superAdminRoutes from "./routes/superAdmin.route.js";
+
 
 dotenv.config({ path: "./.env" });
 // call deatebase connection here
 connectDB();
-
+connectPassport();
 const app = express();
 app.use(cookieParser());
 
@@ -25,16 +33,31 @@ const port = process.env.PORT || 3000;
 app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
+
+// for OAuth 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/api/v1/health", (req, res) => {
   res.status(200).json({ status: "UP", message: "Server is healthy" });
 });
 
 /// root route
- app.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
     message: "Welcome to SkillsMittra API! Server is running.",
@@ -50,8 +73,7 @@ app.use("/api/v1/progress", courseProgressRoute);
 app.use("/api/v1/admin", adminRoutes);
 
 //suprt admin router
-app.use("/api/v1/superadmin",superAdminRoutes)
-
+app.use("/api/v1/superadmin", superAdminRoutes);
 
 app.use("/api/v1/payment", paymentRouter);
 app.get("/home", (req, res) => {
@@ -64,4 +86,3 @@ app.get("/home", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
- 
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -10,6 +10,9 @@ import {
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 import upload from "../utils/multer.js";
 
+import passport from "passport";
+import jwt from "jsonwebtoken";
+
 const router = express.Router();
 
 router.route("/register").post(register);
@@ -19,6 +22,43 @@ router.route("/logout").get(logout);
 
 router.route("/profile").get(isAuthenticated, getUserProfile);
 router.route("/profile/update").put(isAuthenticated,upload.single("profilePhoto") ,   updateProfile);
+
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: `${process.env.CORS_ORIGIN}/login`, 
+    session: false, 
+  }),
+  (req, res) => {
+  
+    const user = req.user;
+
+   
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "15d",
+    });
+
+  
+    const cookieOptions = {
+      httpOnly: true,
+      maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    };
+
+
+    res
+      .cookie("token", token, cookieOptions)
+      .redirect(process.env.CORS_ORIGIN); 
+  }
+);
 
 
 export default router;
